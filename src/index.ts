b104e9254c9ad995bf492a0cf6c4077ddb140480
Federico Moretti <hello@federicomoretti.it>
2025-07-17 19:34:11 +0200

    chore: add lots of [development] dependencies, init TypeScript configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
 #!/usr/bin/env node
+
+import chalk from 'chalk';
+import figlet from 'figlet';
+import { select } from '@inquirer/prompts';
+
+/**
+ * Prints the package name in the console.
+ */
+const init: any = (): void => {
+  console.info(
+    chalk.green(
+      figlet.textSync('jwt-cli', {
+        font: 'Ghost'
+      })
+    )
+  );
+};
+
+/**
+ * 
+ */
+const run: any = (): void => {
+  console.clear();
+  init();
+
+  try {
+    setTimeout(async (): Promise<void> => {
+      console.clear();
+      const answer = await select({
+        message: 'JSON Web Token (JWT) Debugger',
+        choices: [
+          {
+            name: 'JWT Encoder',
+            value: 'encode'
+          },
+          {
+            name: 'JWT Decoder',
+            value: 'decode'
+          }
+        ]
+      });
+      if (answer === 'encode') {
+
+      } else {
+
+      }
+    }, 1000);
+  } catch (error) {
+    console.error(chalk.red.bold(`\n\r${error}`));
+  }
+};
